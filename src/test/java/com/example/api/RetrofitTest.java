86784dcd77baeb54df04bd76af81359f349e4575
savva.genchevskiy <savva.genchevskiy@gmail.com>
2020-06-08 09:36:42 +0300

    UI tests fixes

--- src/test/java/com/example/api/RetrofitTest.java
+++ src/test/java/com/example/api/RetrofitTest.java
@@ -1,8 +1,12 @@
 package com.example.api;
 
+import com.example.api.models.Pet;
+import com.example.api.models.ResponseCode;
 import com.example.api.models.User;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,13 +17,13 @@ public class RetrofitTest extends BaseApiTest {
 
     @Test
     public void shouldCreateUser() throws IOException {
-        var user = User.buildUser().id(0).username(faker.name().username())
+        User user = User.buildUser().id(0).username(faker.name().username())
                 .firstName(faker.name().firstName())
                 .lastName(faker.name().lastName())
                 .email(faker.internet().emailAddress())
                 .password(faker.internet().password(6, 10, true))
                 .userStatus(faker.random().nextInt(10)).build();
-        var response = userService.createUser(user).execute();
+        Response<ResponseCode> response = userService.createUser(user).execute();
         assertAll(
                 ()-> assertNotNull(response.body()),
                 ()-> assertEquals(200, response.code()),
@@ -29,11 +33,11 @@ public class RetrofitTest extends BaseApiTest {
 
     @Test
     public void shouldGetFirstPet() throws IOException {
-        var response = petService.getPet(1).execute();
+        Response<Pet> response = petService.getPet(1).execute();
         assertAll(
                 ()-> assertNotNull(response.body()),
                 ()-> assertEquals(200, response.code()),
-                ()-> assertEquals("ASYDog", response.body().getName())
+                ()-> assertEquals(1, response.body().getId())
         );
     }
 
